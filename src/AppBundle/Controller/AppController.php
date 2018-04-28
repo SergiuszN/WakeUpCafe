ba80023ac8b3ea9d8b2345980948422e710404e5
SergiuszN <serg.ni2011@gmail.com>
2018-04-28 17:11:42 +0200

    Add base user actions

--- src/AppBundle/Controller/AppController.php
+++ src/AppBundle/Controller/AppController.php
@@ -6,8 +6,37 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class AppController extends Controller
 {
+    /**
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
     public function indexAction()
     {
-        return $this->render('@App/layout.html.twig');
+        return $this->render('@App/page/index.html.twig');
+    }
+
+    /**
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function reservationAction()
+    {
+        return $this->render('@App/page/reservation.html.twig');
+    }
+
+    /**
+     * @param int $page
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function blogListAction($page)
+    {
+        return $this->render('@App/page/blogList.html.twig', ['page' => $page]);
+    }
+
+    /**
+     * @param int $id
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function blogPostAction($id)
+    {
+        return $this->render('@App/page/blogPost.html.twig', ['id' => $id]);
     }
 }
